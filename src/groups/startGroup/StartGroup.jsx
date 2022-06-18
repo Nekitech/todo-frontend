7e569aea2666e39b2abd3c61301d782de8bb2a71
Nekitech <beliaew.nikita@mail.ru>
2022-06-18 22:17:02 +0300

    add fon and icons

---
+++ src/groups/startGroup/StartGroup.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './StartGroup.module.css'
+const MyComponent = (props) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+
+
+export default MyComponent;
+
