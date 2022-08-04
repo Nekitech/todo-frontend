599217e56f876535916ba5871dacb2e49a58735b
Nekitech <beliaew.nikita@mail.ru>
2022-08-04 20:02:29 +0300

    delete unnecessary files redux

--- src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-import {groupsReducer} from "./groupsReducer";
-import {menuTaskActiveReducer} from "./menuTaskActiveReducer";
-
-export const rootReducer = combineReducers({
-    groupsReducer: groupsReducer,
-    menuTaskActiveReducer: menuTaskActiveReducer
-});
